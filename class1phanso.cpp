8159b45f0da1f162c263be226a168cc2f30c2f1d
hari <tranvanhaidtvt@gmail.com>
2023-06-10 15:26:35 +0700

    update for plus phanso

--- class1phanso.cpp
+++ class1phanso.cpp
@@ -8,6 +8,10 @@ int ucnl(int a, int b)
     if (b == 0)   return a;
     return ucnl(b, a%b);
 }
+int bcnn(int a, int b)
+{
+    return (a*b)/ucnl(a,b);
+}
 class Phanso
 {
     private:
@@ -16,8 +20,10 @@ class Phanso
     public:
         Phanso(int,int);
         void rutgon();
+        void tong();
         friend istream& operator >> (istream& in, Phanso& a);
         friend ostream& operator << (ostream& out, Phanso a);
+        friend Phanso operator + (Phanso, Phanso);
 };
 Phanso::Phanso(int a, int b)
 {
@@ -40,11 +46,21 @@ void Phanso::rutgon()
     this->tu = tu/tmp;
     this->mau = mau/tmp;
 }
+Phanso operator + (Phanso a, Phanso b)
+{
+    Phanso tong(1,1);
+    int tmp = bcnn(a.mau,b.mau);
+    cout << tmp << endl;
+    tong.mau = tmp;
+    tong.tu = (tmp/a.mau)*a.tu + (tmp/b.mau)*b.tu;
+    return tong;
+}
 int main()
 {
-    Phanso p(1,1);
-    cin >> p;
-    p.rutgon();
-    cout << p;
+    Phanso p(1,1), q(1,1);
+    cin >> p >> q;
+    Phanso tong = p+q;
+    // tong.rutgon();
+    cout << tong;
     return 0;
 }
